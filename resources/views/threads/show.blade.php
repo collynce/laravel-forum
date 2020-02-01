8b6f0210665e8971f4eabcb8e984c8f4266f1cdf
Collins Kiprotich <collinsrono02@gmail.com>
2020-02-01 15:23:21 +0300

    Episode 5: The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -18,12 +18,25 @@
                     </div>
 
                     @foreach($thread->replies as $reply)
-                            <div class="card mb-4">
-                                <div class="card-header"><span><a href="">{{$reply->owner->name}}</a> said </span>
-                                    {{$reply->created_at->diffForHumans()}}...</div>
-                                @include('threads.reply')
+                        <div class="card mb-4">
+                            <div class="card-header"><span><a href="">{{$reply->owner->name}}</a> said </span>
+                                {{$reply->created_at->diffForHumans()}}...
                             </div>
+                            @include('threads.reply')
+                        </div>
                     @endforeach
+
+                    @if(auth()->check())
+                        <div class="row justify-content-center">
+                            <div class="col-md-8 col-md-offset-2">
+                                <form class="form-group" action="{{$thread->path().'/replies'}}" method="post">
+                                    @csrf
+                                    <textarea name="body" id="body" class="form-control" cols="30" rows="8" placeholder="Say something"></textarea>
+                                    <button class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
