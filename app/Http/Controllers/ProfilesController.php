b2b021b5fcc07dc07f5dee63d2bc0846f8f0e8ac
Collins Kiprotich <collinsrono02@gmail.com>
2020-02-03 01:10:20 +0300

    Episode 26: How to Construct An Activity Feed with TDD: Part 2

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -46,9 +47,12 @@ class ProfilesController extends Controller
      */
     public function show(User $user)
     {
+        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity){
+            return $activity->created_at->format('Y-m-d');
+        });
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(28)
+            'activities' => $activities
         ]);
     }
 
