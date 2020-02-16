7faf8f890820d6adc47486f606ca3f6e4347636f
Collins Rono <collinsrono02@gmail.com>
2020-02-16 21:19:06 +0300

    Episode 37: A New Reply Component

--- resources/js/components/Replies.vue
+++ resources/js/components/Replies.vue
@@ -3,14 +3,17 @@
         <div v-for="(reply, index) in items">
             <reply :data="reply" @deleted="remove(index)"></reply>
         </div>
+
+        <new-reply :endpoint="endpoint" @created="add"></new-reply>
     </div>
 </template>
 
 <script>
     import Reply from './Reply.vue';
+    import NewReply from './NewReply.vue';
     export default {
         props: ['data'],
-        components: { Reply},
+        components: { Reply, NewReply },
         data() {
             return {
                 items: this.data,
