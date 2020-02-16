fef8e38d0ec75e5c5b30dde7ec95b82e6efa507d
Collins Rono <collinsrono02@gmail.com>
2020-02-16 14:37:25 +0300

    Episode 35: Squashing Bugs

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -7,9 +7,16 @@ namespace App;
 trait Favoritable
 {
 
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
+
     public function isFavorited()
     {
-        return !! $this->favorites->where('user_id', auth()->id())->count();
+        return ! ! $this->favorites->where('user_id', auth()->id())->count();
     }
 
     public function favorites()
@@ -21,12 +28,13 @@ trait Favoritable
     {
         $attributes = ['user_id' => auth()->id()];
 
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
     }
 
     public function getIsFavoritedAttribute()
     {
         return $this->isFavorited();
+
     }
 
     public function favorite()
