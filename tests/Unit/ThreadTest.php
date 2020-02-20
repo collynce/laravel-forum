e76fa25af23ae7a2649b2932afbebd40f110f7be
Collins Rono <collinsrono02@gmail.com>
2020-02-20 08:54:21 +0300

    Episode 43: Thread Subscriptions: Part 4

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -57,11 +57,8 @@ class ThreadTest extends TestCase
     function a_thread_can_be_unsubscribed_from()
     {
         $thread = create('App\Thread');
-
         $thread->subscribe($userId = 1);
-
         $thread->unsubscribe($userId);
-
         $this->assertCount(0, $thread->subscriptions);
     }
 
@@ -69,13 +66,9 @@ class ThreadTest extends TestCase
     function it_knows_if_the_authenticated_user_is_subscribed_to_it()
     {
         $thread = create('App\Thread');
-
         $this->signIn();
-
         $this->assertFalse($thread->isSubscribedTo);
-
         $thread->subscribe();
-
         $this->assertTrue($thread->isSubscribedTo);
     }
 }
